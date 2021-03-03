9fe2ff8cd73a204ffbcd25f8d6c4b10324ca2177
grejioh <grejioh@outlook.com>
2021-03-03 13:06:17 +0800

    first commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(pyim-dicts
+   '((:name "pyim-bigdict" :file "/home/grejioh/.doom.d/PyimDict/pyim-bigdict.pyim.gz"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
